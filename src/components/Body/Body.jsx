64529b3502c29392806166a260451e77489834ca
Tshewang  Gyaltshen <tshewanggyaltshen0@gmail.com>
2023-09-16 23:19:58 +0600

    added all the images

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,14 +1,91 @@
 const Body = ()=>{
     return(
         <>
-            <div className="body">
-                <div className="w-full h-[15rem] bg-red-100 gap-y-4">will contain the photo</div>
-            </div>
-            <div className="grid grid-cols-4 gap-4 mx-[10px] bg-red-100">
-                <div className="h-[25rem] bg-slate-400 rounded-md">col 1</div>
-                <div className="h-[25rem] bg-slate-400 rounded-md">col 2</div>
-                <div className="h-[25rem] bg-slate-400 rounded-md">col 3</div>
-                <div className="h-[25rem] bg-slate-400 rounded-md">col 4</div>
+            <div className="bg-red-100">
+
+                <div className="body">
+                    <div className="w-full h-[15rem] gap-y-4 grid grid-cols-4">
+                        <div></div>
+                        <div>
+                            <img src="/bg-rm-14.png" alt="bg" className="h-[15rem] w-full" />
+                        </div>
+                        <div className="mt-[5rem]">
+                            <h3 className="text-2xl text-center">WONDE WOMEN FEST</h3>
+                            <p className="text-extrabold text-center">--31 jan-31 Dec--</p>
+                            <h3 className="text-2xl text-center">UP TO 30% OFF</h3>
+                            <div className="bg-white w-[80%] rounded-md ml-[2rem] h-[2rem]">
+                                <h4 className="text-center">EXRRA 10% CASH BACK</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-4 gap-4 mx-[10px]">
+                    <div className="h-[25rem] rounded-[4px] bg-white">
+                        <h4 className="text-center w-full h-[6rem] font-bold pt-[10px]">
+                            Triple camera Smartphones | Starting $40
+                        </h4>
+                        <div className="grid grid-cols-2 gap-[1rem] m-[4px] ml-[1rem] mt-[1rem]">
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/1.jpg" alt="" className="h-[8rem] rounded-md" />
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/2.jpg" alt="" className="h-[8rem] w-full rounded-md"/>
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="3.jpg" alt="" className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/4.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[25rem] rounded-md bg-white">
+                    <h4 className="text-center w-full h-[6rem] font-bold pt-[10px]">Up to 45% off TVs & appliances</h4>
+                        <div className="grid grid-cols-2 gap-[1rem] m-[4px] ml-[1rem] mt-[1rem]">
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/5.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/> 
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/6.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/> 
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/7.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/8.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[25rem] rounded-md bg-white">
+                    <h4 className="text-center w-full h-[6rem] font-bold pt-[10px]">Recharges, bills, movies & more</h4>
+                        <div className="grid grid-cols-2 gap-[1rem] m-[4px] ml-[1rem] mt-[1rem]">
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/9.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/10.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/11.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                            <div className="w-[8rem] h-[8rem] rounded-[4px]">
+                                <img src="/12.jpg" alt=""  className="resize rounded-md w-[100%] h-[8rem]"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[25rem] rounded-md bg-white">
+                        <div>
+                            <p className="text-center w-full h-[6rem] bg-orange-400 font-bold pt-[10px] bg-white">Sign in for your best experiences</p>
+                            <button className="w-[97%] ml-[4px] bg-amber-400 rounded-[6px] h-[2rem] ring-1">
+                                sign in security
+                            </button>
+                            <div className="w-[90%] h-[15rem] mx-[1rem] mt-[2rem] bg-green-200 rounded-[6px]">
+                                <p className="font-extrabold text-center">World is never <br/> far away</p>
+                                <img src="/13.jpg" alt="image" className="h-[11rem]"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
